ef22efe7e2454b76e244874438019ecc7b9a08c8
killifranko <igenbaev2k@gmail.com>
2019-10-16 21:02:15 +0600

    1

--- printalphabet/main.go
+++ printalphabet/main.go
@@ -2,12 +2,13 @@ package main
 
 import "github.com/01-edu/z01"
 
-func main () {
+func main() {
 
-	for i == 'a'; i<= 'z'; i++ {
-		z01.PrintRune(1)
+	for i := 97; i <= 122; i++ {
+
+		z01.PrintRune(rune(i))
 
 	}
-	z01.PrintRune(10)
+	z01.PrintRune('\n')
 
 }
