f62b708a392b176864abfe803c23c9f6abbfed47
killifranko <igenbaev2k@gmail.com>
2019-10-16 21:52:48 +0600

    22

--- printreversealphabet/main.go
+++ printreversealphabet/main.go
@@ -4,9 +4,9 @@ import "github.com/01-edu/z01"
 
 func main() {
 
-	for i := 122; i >= 97; i-- {
+	for i := 'z'; i >= 'a'; i-- {
 
-		z01.PrintRune(rune(i))
+		z01.PrintRune((i))
 	}
 	z01.PrintRune(10)
 
